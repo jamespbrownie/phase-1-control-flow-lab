9b07ba2f3aa4be211c0864885c7b9ad224c27e77
James Brownie <jamespbrownie@gmail.com>
2021-09-20 17:18:48 -0500

    commit tree 1ea03aa15b8b8336892a0a83f3aad7a44e5af064

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  if (i <= 400) {
+    return "This one is on me!"
+  } else if (i > 2000 && i < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (i > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return (city == "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(i){
+  switch (i) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
